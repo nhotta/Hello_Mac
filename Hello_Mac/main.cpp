af716a1f8fee3df4586a2add964b41b069a8eb2c
Naoki Hotta <naokihot@yahoo.com>
2011-10-25 18:48:26 -0700

    this is easier

--- Hello_Mac/main.cpp
+++ Hello_Mac/main.cpp
@@ -6,13 +6,13 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
 
 int main (int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("Hello, World!\n");
     return 0;
 }
 
