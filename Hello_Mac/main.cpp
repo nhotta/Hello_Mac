7e43252e91b87eee2bf05ed74d352c5bd3f082f3
Naoki Hotta <naokihot@yahoo.com>
2011-10-25 18:35:28 -0700

    Initial Commit

---
+++ Hello_Mac/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello_Mac
+//
+//  Created by Naoki Hotta on 10/25/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
